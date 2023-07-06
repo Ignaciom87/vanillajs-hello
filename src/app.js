f97226ff81fb3ffcdc6347f72958797cd3a6c774
Ignaciom87 <117761825+Ignaciom87@users.noreply.github.com>
2023-07-06 14:54:19 +0000

    Domain generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,162 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let avaiblePronoums = [
+    "vosotros",
+    "ustedes",
+    "el",
+    "ella",
+    "nosotros",
+    "vosotras"
+  ];
+  let avaibleAdjetives = [
+    "amable",
+    "generoso",
+    "enérgico",
+    "motivador",
+    "experto"
+  ];
+  let avaibleExtensions = [".com", ".es", ".biz", ".info", ".fr", ".eu"];
+
+  // Mensajes de alerta
+
+  let ALERT_MISSING_CHECKBOX = `
+  <div class="alert alert-danger" role="alert">
+    POR FAVOR SELECCIONE AL MENOS UNA EXTENSION DE DOMINIO
+  </div>
+  `;
+
+  let ALERT_MISSING_NAME = `
+  <div class="alert alert-danger" role="alert">
+    INTRODUZCA AL MENOS UN NOMBRE.
+  </div>
+  `;
+
+  // Listener del boton
+  let rawForm = document.getElementById("formOptions");
+  rawForm.addEventListener("submit", recibirDatosForm);
+
+  function recibirDatosForm(e) {
+    e.preventDefault();
+    let listaPronombres = readCheckboxValue(avaiblePronoums);
+    let listaAdjetivos = readCheckboxValue(avaibleAdjetives);
+    let listaNombres = readValueFromId("productNames")
+      .replace(/\s/g, "")
+      .split(",");
+    let listaExtensiones = readCheckboxValue(avaibleExtensions);
+
+    let domainListHtml = generateDomainList({
+      pronoums: listaPronombres,
+      adjetives: listaAdjetivos,
+      names: listaNombres,
+      extensions: listaExtensiones
+    });
+
+    if (listaExtensiones.length == 0) {
+      domainListHtml = ALERT_MISSING_CHECKBOX;
+    }
+    if (listaNombres[0] == "") {
+      domainListHtml = ALERT_MISSING_NAME;
+    }
+    writeToId("domainBox", domainListHtml);
+  }
+
+  let writeToId = (searchedID, strToWrite) => {
+    document.getElementById(searchedID).innerHTML = strToWrite;
+  };
+
+  let readCheckboxValue = listName => {
+    let valueList = [];
+    for (let itemName of listName) {
+      let item = document.getElementById(itemName);
+      if (item.checked) {
+        valueList.push(item.value);
+      }
+    }
+    return valueList;
+  };
+
+  function generateDomainList(objetoForm) {
+    let domainBoxHtml = ``;
+    let domainBoxHtmlHack = ``;
+    if (!objetoForm.pronoums[0]) {
+      objetoForm.pronoums.push("");
+    }
+    if (!objetoForm.adjetives[0]) {
+      objetoForm.adjetives.push("");
+    }
+    for (let pronombre of objetoForm.pronoums) {
+      for (let adjetivo of objetoForm.adjetives) {
+        for (let nombre of objetoForm.names) {
+          for (let extension of objetoForm.extensions) {
+            domainBoxHtml += `
+              <p class="my-1 border bg-success border-success rounded">
+                <a
+                  class="text-decoration-none text-light"
+                  href="http://${pronombre}${adjetivo}${nombre}${extension}"
+                  >
+                  ${pronombre}${adjetivo}${nombre}${extension}  
+                </a>
+              </p>        
+            `;
+            if (tryDomainNameHack(nombre, extension)) {
+              let compareExtension = extension.slice(1);
+              let hackedName = nombre.slice(
+                0,
+                nombre.length - compareExtension.length
+              );
+
+              domainBoxHtmlHack += `
+                <p class="my-1 border bg-success border-success rounded">
+                  RECOMMENDED |-->  <a
+                    class="text-decoration-none text-light"
+                    href="http://${pronombre}${adjetivo}${hackedName}${extension}"
+                    >
+                    ${pronombre}${adjetivo}${hackedName}${extension}  
+                  </a>  <--| RECOMMENDED
+                </p>        
+              `;
+            }
+          }
+        }
+      }
+    }
+    return domainBoxHtmlHack + domainBoxHtml;
+  }
+
+  function tryDomainNameHack(name, extension) {
+    if (
+      name.slice(name.length - (extension.length - 1)) == extension.slice(1)
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  let readValueFromId = searchedID => document.getElementById(searchedID).value;
+
+  let createCheckForms = (listWanted, wantedID) => {
+    let allChecks = "";
+    for (let avaibleThing of listWanted) {
+      allChecks += `
+      <div>
+        <input
+          class="form-check-input "
+          type="checkbox"
+          value="${avaibleThing}"
+          id="${avaibleThing}"
+        />
+        <label class="form-check-label" for="${avaibleThing}">
+          ${avaibleThing}
+        </label>
+      </div>
+    `;
+    }
+    writeToId(wantedID, allChecks);
+  };
+
+  createCheckForms(avaiblePronoums, "pronoumListForm");
+  createCheckForms(avaibleAdjetives, "adjetivesListForm");
+  createCheckForms(avaibleExtensions, "extensionListForm");
 };
+
